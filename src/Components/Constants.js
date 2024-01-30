b44daba52ea500be5ad57857c8313d24b79ed5a7
ayasophya <jadekamsing@gmail.com>
2024-01-30 09:58:11 -0500

    test3

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,10 +1,9 @@
-export let CarDetails = {
-    make: '',
-    model: '',
-    year: '',
-    exist: false
+export const CarDetails = {
+  make: '',
+  model: '',
+  year: '',
+  exist: false
 }
 
 export const APIBaseUrl = 'https://cwcp-backend-api.onrender.com/api/v1'
 export const APIDomain = 'https://cwcp-backend-api.onrender.com'
-
