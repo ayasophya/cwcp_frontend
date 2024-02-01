b4444592658894ccf09770e1ea4bfd8b110ff833
ayasophya <jadekamsing@gmail.com>
2024-01-31 23:52:04 -0500

    removed localhost

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -5,7 +5,5 @@ export const CarDetails = {
   exist: false
 }
 
-//export const APIBaseUrl = 'https://cwcp-backend-api.onrender.com/api/v1'
-//export const APIDomain = 'https://cwcp-backend-api.onrender.com'
-export const APIBaseUrl = 'http://localhost:8080/api/v1'
-export const APIDomain = 'http://localhost:8080'
+export const APIBaseUrl = 'https://cwcp-backend-api.onrender.com/api/v1'
+export const APIDomain = 'https://cwcp-backend-api.onrender.com'
