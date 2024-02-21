9a58616501eaf0795a99a17fe32620000defd4f0
SamuelNguyen2121 <2032373@champlaincollege.qc.ca>
2024-02-21 15:31:58 -0500

    Use deployed backend

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -5,7 +5,7 @@ export const CarDetails = {
   exist: false
 }
 
- //export const APIBaseUrl = 'https://api.canadawidecarparts.tech/api/v1'
- //export const APIDomain = 'https://api.canadawidecarparts.tech'
-  export const APIBaseUrl = 'http://localhost:8080/api/v1'
-  export const APIDomain = 'http://localhost:8080'
+ export const APIBaseUrl = 'https://api.canadawidecarparts.tech/api/v1'
+ export const APIDomain = 'https://api.canadawidecarparts.tech'
+//  export const APIBaseUrl = 'http://localhost:8080/api/v1'
+//  export const APIDomain = 'http://localhost:8080'
